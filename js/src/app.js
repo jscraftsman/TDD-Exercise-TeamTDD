0ef5e939fd0de7f9a99b215434cf826d70829cb1
Gervel Giva <gervelgiva@gmail.com>
2018-09-07 18:58:09 +0800

    Refactored App.start()

--- js/src/app.js
+++ js/src/app.js
@@ -1,23 +1,31 @@
 (() => {
     const App = {
-        INPUT_MODULE_MISSING_ERROR: 'InputModule is missing!',
-        CPU_MODULE_MISSING_ERROR: 'CPUModule is missing!',
-        OUTPUT_MODULE_MISSING_ERROR: 'OutputModule is missing!',
+        ERRORS: {
+            INVALID_INITIALIZATION: 'Invalid initialziation of App.start()! Modules must be passed as an argument.',
+            INPUT_MODULE_MISSING: 'InputModule is missing!',
+            CPU_MODULE_MISSING: 'CPUModule is missing!',
+            OUTPUT_MODULE_MISSING: 'OutputModule is missing!'
+        },
 
-        start: function () {
-            throwIfAModuleIsMissing();
+        start: function (modules) {
+            throwIfAModuleIsMissing(modules);
+            MODULES = modules;
         }
     };
 
+    /* Private variables */
+    let MODULES = null;
+
     /* Private functions */
-    function throwIfAModuleIsMissing() {
-        if (InputModule === undefined) throw App.INPUT_MODULE_MISSING_ERROR;
-        if (CPUModule === undefined) throw App.CPU_MODULE_MISSING_ERROR;
-        if (OutputModule === undefined) throw App.OUTPUT_MODULE_MISSING_ERROR;
+    function throwIfAModuleIsMissing(modules) {
+        if (modules === undefined) throw App.ERRORS.INVALID_INITIALIZATION;
+        if (modules.InputModule === undefined) throw App.ERRORS.INPUT_MODULE_MISSING;
+        if (modules.CPUModule === undefined) throw App.ERRORS.CPU_MODULE_MISSING;
+        if (modules.OutputModule === undefined) throw App.ERRORS.OUTPUT_MODULE_MISSING;
     }
 
     window.App = App;
 
-    // TODO: Create "Main" and use App.start();
+    // TODO: Create "Main" and use App.start(); Pass modules as parameter
     // TODO: Add UI Interface in Main
 })();
--- js/test/app.test.js
+++ js/test/app.test.js
@@ -10,47 +10,71 @@
     });
 
     QUnit.test("App has a list of static error messages", (assert) => {
-        assert.deepEqual(App.INPUT_MODULE_MISSING_ERROR, "InputModule is missing!");
-        assert.deepEqual(App.CPU_MODULE_MISSING_ERROR, "CPUModule is missing!");
-        assert.deepEqual(App.OUTPUT_MODULE_MISSING_ERROR, "OutputModule is missing!");
+        assert.deepEqual(
+            App.ERRORS.INVALID_INITIALIZATION,
+            "Invalid initialziation of App.start()! Modules must be passed as an argument.");
+        assert.deepEqual(App.ERRORS.INPUT_MODULE_MISSING, "InputModule is missing!");
+        assert.deepEqual(App.ERRORS.CPU_MODULE_MISSING, "CPUModule is missing!");
+        assert.deepEqual(App.ERRORS.OUTPUT_MODULE_MISSING, "OutputModule is missing!");
     });
 
-    QUnit.test("App.start() throws exception if InputModule is missing", (assert) => {
+    QUnit.test("App.start() throws exception if has an empty argument", (assert) => {
         function functionBlock() {
-            InputModule = undefined; // Intentinally remove instance
             App.start();
         }
 
         function expectedMatcher(err) {
-            return err === App.INPUT_MODULE_MISSING_ERROR;
+            return err === App.ERRORS.INVALID_INITIALIZATION;
+        }
+        assert.throws(functionBlock, expectedMatcher);
+    });
+
+    QUnit.test("App.start() throws exception if InputModule is missing", (assert) => {
+        function functionBlock() {
+            let mockModules = {
+                InputModule: undefined, // Intentinally remove instance
+                CPUModule: {}, // Mock module
+                OutputModule: {} //  Mock module
+            };
+            App.start(mockModules);
+        }
+
+        function expectedMatcher(err) {
+            return err === App.ERRORS.INPUT_MODULE_MISSING;
         }
         assert.throws(functionBlock, expectedMatcher);
     });
 
     QUnit.test("App.start() throws exception if CPUModule is missing", (assert) => {
         function functionBlock() {
-            InputModule = {}; // Mock InputModule
-            CPUModule = undefined; // Intentinally remove instance
-            App.start();
+            let mockModules = {
+                InputModule: {}, //  Mock module
+                CPUModule: undefined, // Intentinally remove instance
+                OutputModule: {} //  Mock module
+            };
+            App.start(mockModules);
         }
 
         function expectedMatcher(err) {
-            return err === App.CPU_MODULE_MISSING_ERROR;
+            return err === App.ERRORS.CPU_MODULE_MISSING;
         }
         assert.throws(functionBlock, expectedMatcher);
     });
 
     QUnit.test("App.start() throws exception if OutputModule is missing", (assert) => {
         function functionBlock() {
-            InputModule = {}; // Mock InputModule
-            CPUModule = {}; // Mock CPUModule
-            OutputModule = undefined; // Intentinally remove instance
-            App.start();
+            let mockModules = {
+                InputModule: {}, //  Mock module
+                CPUModule: {}, //  Mock module
+                OutputModule: undefined // Intentinally remove instance
+            };
+            App.start(mockModules);
         }
 
         function expectedMatcher(err) {
-            return err === App.OUTPUT_MODULE_MISSING_ERROR;
+            return err === App.ERRORS.OUTPUT_MODULE_MISSING;
         }
         assert.throws(functionBlock, expectedMatcher);
     });
+
 })();
