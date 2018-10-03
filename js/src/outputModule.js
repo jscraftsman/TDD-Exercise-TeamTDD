f850e352bfc488811882ea6e83bd7e4955e327a7
Gervel Giva <gervelgiva@gmail.com>
2018-10-03 18:08:37 +0800

    Initial implementation for display map for OutputModule

--- js/src/outputModule.js
+++ js/src/outputModule.js
@@ -4,6 +4,26 @@
     'use strict';
 
     const OutputModule = {
+        DISPLAY_MAP: {
+            NEGATIVE: 'g',
+            EMPTY: 'empty',
+            ZERO: 'abcdef',
+            ONE: 'bc',
+            TWO: 'abdeg',
+            THREE: 'abcdg',
+            FOUR: 'bcfg',
+            FIVE: 'acdfg',
+            SIX: 'acdefg',
+            SEVEN: 'abc',
+            EIGHT: 'abcdefg',
+            NINE: 'abcfg',
+            ZERO_POINT: 'abcdefh',
+            ONE_POINT: 'bch',
+            TWO_POINT: 'abdegh',
+            THREE_POINT: 'abcdgh',
+            FOUR_POINT: 'bcfgh'
+        },
+
         RESULT: 0,
 
         setResult: function (newValue) {
--- js/test/outputModule.test.js
+++ js/test/outputModule.test.js
@@ -17,6 +17,26 @@
         assert.equal(OutputModule.RESULT, expectedValue);
     });
 
+    QUnit.test('has the necessary display map', assert => {
+        assert.equal(OutputModule.DISPLAY_MAP.NEGATIVE, 'g');
+        assert.equal(OutputModule.DISPLAY_MAP.EMPTY, 'empty');
+        assert.equal(OutputModule.DISPLAY_MAP.ZERO, 'abcdef');
+        assert.equal(OutputModule.DISPLAY_MAP.ONE, 'bc');
+        assert.equal(OutputModule.DISPLAY_MAP.TWO, 'abdeg');
+        assert.equal(OutputModule.DISPLAY_MAP.THREE, 'abcdg');
+        assert.equal(OutputModule.DISPLAY_MAP.FOUR, 'bcfg');
+        assert.equal(OutputModule.DISPLAY_MAP.FIVE, 'acdfg');
+        assert.equal(OutputModule.DISPLAY_MAP.SIX, 'acdefg');
+        assert.equal(OutputModule.DISPLAY_MAP.SEVEN, 'abc');
+        assert.equal(OutputModule.DISPLAY_MAP.EIGHT, 'abcdefg');
+        assert.equal(OutputModule.DISPLAY_MAP.NINE, 'abcfg');
+        assert.equal(OutputModule.DISPLAY_MAP.ZERO_POINT, 'abcdefh');
+        assert.equal(OutputModule.DISPLAY_MAP.ONE_POINT, 'bch');
+        assert.equal(OutputModule.DISPLAY_MAP.TWO_POINT, 'abdegh');
+        assert.equal(OutputModule.DISPLAY_MAP.THREE_POINT, 'abcdgh');
+        assert.equal(OutputModule.DISPLAY_MAP.FOUR_POINT, 'bcfgh');
+    });
+
     QUnit.test('has a getLeftDigit() function that returns the left digit of a two digit result', assert => {
         let mockResult = 12;
         let expectedValue = 1;
@@ -37,8 +57,19 @@
         assert.equal(rightDigit, expectedValue);
     });
 
-    QUnit.todo('1 digit result, negative result, decimal result', assert => {
+    QUnit.todo('has a getLeftDigit() and getRightDigit() function that returns a one digit result', assert => {
+        let mockResult = 1;
 
+        OutputModule.setResult(mockResult);
+        let leftDigit = OutputModule.getLeftDigit();
+        let rightDigit = OutputModule.getRightDigit();
+
+        assert.equal(leftDigit, OutputModule.DISPLAY_MAP.EMPTY);
+        assert.equal(rightDigit, OutputModule.DISPLAY_MAP.ZERO);
     });
+
+    // TODO: 1 digit result
+    // TODO: negative result
+    // TODO: decimal result
 
 })();
